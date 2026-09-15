dfe63f16c7ca43cea154165bd60f6f849508fd0d
miguelenes <1568086+miguelenes@users.noreply.github.com>
2026-09-15 13:30:30 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,7 +3,7 @@
 use Eloquage\Chunk\Chunk;
 
 it('bootstraps the package entrypoint', function () {
-    $instance = new Chunk();
+    $instance = new Chunk;
 
     expect($instance->name())->toBe('chunk');
 });
